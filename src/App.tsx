b2119b061e058ad2300f39ff836af15d5d87055e
jiji <pagwa334@gmail.com>
2024-12-31 14:55:08 +0900

    ✅ feat : 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./Layout";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={"/홈"} />
+          <Route path="/feeds/:id" element={"/상세페이지"} />
+          <Route path="/feeds/create" element={"/추가페이지"} />
+          <Route path="/mypage" element={"/마이 페이지"} />
+        </Route>
+        <Route path="/signup" element={"/회원가입 페이지"} />
+        <Route path="/login" element={"/로그인 페이지"} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
