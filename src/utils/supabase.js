7b2dfcdfbe3a8fa41281c80ceb11756ea4ff727b
jiji <pagwa334@gmail.com>
2024-12-31 14:24:26 +0900

    feat :초기세팅

---
+++ src/utils/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://hmhwazxprvwmwtwukfxu.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+//기존에 있던걸 바꾸기!
+// /VITE_SUPABASE_KEY를 env파일로
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
