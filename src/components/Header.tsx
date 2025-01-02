6c758d6ac51d7526c5dec459b5bd7f4de6fba200
jiji <pagwa334@gmail.com>
2025-01-02 17:15:49 +0900

    ♻️ 코드 리팩토링

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,66 +1,53 @@
-import React, { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import supabase from "../utils/supabase";
 
-const Header = () => {
-  const [user, setUser] = useState(null); // 유저 상태 관리
+export default function Header() {
+  // const user = {
+  // 	nickname: "홍길동",
+  // };
+  const user = null;
 
   const handleLogout = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (!error) {
-      setUser(null); // 유저 상태 초기화
-      alert("로그아웃되었습니다.");
-    } else {
-      console.error("로그아웃 실패:", error.message);
-    }
+    await supabase.auth.signOut();
   };
-
-  return user ? (
-    <header className="flex justify-between items-center p-4 border-b border-gray-200 bg-white mb-10">
-      <div>
-        <a href="/" data-discover="true">
+  return (
+    <>
+      <header className="flex justify-between bg-white p-4 border-b border-gray-200 items-center">
+        <Link to="/">
           <img src="/logo.svg" alt="logo" />
-        </a>
-      </div>
-      <div className="flex items-center gap-2">
-        <a
-          href="/mypage"
-          data-discover="true"
-          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-        >
-          {/* {user.nickname || "마이페이지"} */}
-        </a>
-        <button
-          onClick={handleLogout}
-          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-        >
-          로그아웃
-        </button>
-      </div>
-    </header>
-  ) : (
-    <header className="flex justify-between items-center p-4 border-b border-gray-200 bg-white mb-10">
-      <div>
-        <a href="/" data-discover="true">
-          <img src="/logo.svg" alt="logo" />
-        </a>
-      </div>
-      <div className="flex items-center gap-2">
-        <a
-          href="/login"
-          data-discover="true"
-          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-        >
-          로그인
-        </a>
-        <a
-          href="/signup"
-          data-discover="true"
-          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-        >
-          회원가입
-        </a>
-      </div>
-    </header>
+        </Link>
+        <div className="flex gap-2 items-center">
+          {user ? (
+            <>
+              <Link to="/mypage" className="hover:underline">
+                {user.nickname}
+              </Link>
+              <button
+                onClick={handleLogout}
+                className="text-sm text-gray-900 bg-white border border-gray-300 rounded-lg px-5 py-2.5 hover:bg-gray-100 font-medium"
+              >
+                로그아웃
+              </button>
+            </>
+          ) : (
+            <>
+              <Link
+                to="/login"
+                className="text-sm text-gray-900 bg-white border border-gray-300 rounded-lg px-5 py-2.5 hover:bg-gray-100 font-medium"
+              >
+                로그인
+              </Link>
+              <Link
+                to="/signup"
+                className="text-sm text-gray-900 bg-white border border-gray-300 rounded-lg px-5 py-2.5 hover:bg-gray-100 font-medium"
+              >
+                회원가입
+              </Link>
+            </>
+          )}
+        </div>
+      </header>
+      <Outlet />
+    </>
   );
-};
-
-export default Header;
+}
