e65a4e718013b910199125be45be365b029916de
jiji <pagwa334@gmail.com>
2025-01-08 11:52:34 +0900

    ✅ feat : 홈 Ui 생성 및 FeedForm 컴포넌트 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import FeedForm from "../components/FeedForm";
 
 const Home = () => {
   return (
-    <div className="text-base text-red-200">
-Hello world!
+    <div className="max-w-screen-lg mx-auto min-h-[cc(100vh-100px)] px-10 mb-10:">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-bold">글 목록</h1>
+        <Link
+          to="/feeds/create"
+          className="bg-white text-gray-900 px-4 py-2 rounded-md"
+        >
+          글쓰기
+        </Link>
+      </div>
+      <div>
+        <FeedForm/>
+      </div>
     </div>
   );
 };
