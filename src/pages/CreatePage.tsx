9ac14f3ca581f81b451ccb8c93a674657a1bd0f4
jiji <pagwa334@gmail.com>
2025-01-10 11:48:17 +0900

    ✅ CreatPage,UpdatePage 라우터 및 페이지 추가

---
+++ src/pages/CreatePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FeedForm from "../components/FeedForm";
+
+const CreatePage = () => {
+  return <FeedForm />;
+};
+
+export default CreatePage;
---
+++ src/pages/UpdatePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FeedForm from "../components/FeedForm";
+
+const UpdatePage = () => {
+  return <FeedForm />;
+};
+
+export default UpdatePage;
